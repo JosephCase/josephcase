3a1ac2bf30a7bf13bde5dfb45bc2a2d7ebde669b
Joseph Case <joseph.case@hotmail.co.uk>
2015-05-13 21:59:47 +0100

    changing directory

--- js/bubbles.js
+++ js/bubbles.js
@@ -8,16 +8,25 @@ var Bubbles = function() {
 	var me = this;
 
 	this.contruct = function() {
-
+		$("header").on("click", function() {
+			me.closeBubbles();
+		})
 		$(".bubble").on("click", function() {
-			me.bubbleClick(event);
+			me.openBubble(event);
 		})
 	}
 
-	this.bubbleClick = function(event) {
+	this.openBubble = function(event) {
 
 		$target = $(event.target);
-		$target.toggleClass("show");
+		$target.addClass("show");
+		$("header").addClass("active");
+	}
+	this.closeBubbles = function() {
+
+		$("header").addClass("active");
+		$target = $(".bubble");
+		$target.removeClass("show");
 	}	
 
 	this.contruct();
