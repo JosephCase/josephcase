ef9bdf4c7ebdfdd37a6a806c3c1323980e34b74b
Joseph Case <joseph.case@hotmail.co.uk>
2015-05-14 02:15:14 +0100

    working with iframe

--- js/bubbles.js
+++ js/bubbles.js
@@ -5,7 +5,8 @@ $( document ).ready(function() {
 
 
 var Bubbles = function() {
-	var me = this;
+	var me = this,
+		timeout;
 
 	this.contruct = function() {
 		$("header").on("click", function() {
@@ -18,16 +19,48 @@ var Bubbles = function() {
 
 	this.openBubble = function(event) {
 
+
 		$target = $(event.target);
+		$target.unbind();
 		$target.addClass("show");
 		$("header").addClass("active");
+
+		$target.on("mousedown", function () {
+			me.resizeStart($target);
+		});
+		$target.on("mouseup", function () {
+			me.resizeEnd($target);
+		})
+
 	}
 	this.closeBubbles = function() {
 
 		$("header").addClass("active");
 		$target = $(".bubble");
 		$target.removeClass("show");
-	}	
+	}
+
+	this.resizeStart = function($target) {
+		$target.unbind("mousedown");
+
+		$target.on("mousemove", function () {
+			me.resize(event);
+		});
+	}
+
+	this.resize = function(event) {
+		console.log("resize");
+		$iframe = $(event.target).children("iframe");
+		$iframe.css("width", event.pageX + "px");
+	}
+
+	this.resizeEnd = function($target) {
+		$target.unbind("mousemove");
+		$target.on("mousedown", function () {
+			me.resizeStart($target);
+		});
+
+	}			
 
 	this.contruct();
 }
