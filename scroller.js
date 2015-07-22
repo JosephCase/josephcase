53a88e3bf856166e9d9fa4026892d68c4446a1b8
Joseph Case <joseph.case@hotmail.co.uk>
2015-07-22 20:50:25 +0100

    fixed scrolling mystery

--- scroller.js
+++ scroller.js
@@ -6,15 +6,20 @@ function Scroller() {
 
 	attachScroll();
 
+	// var rect = element.getBoundingClientRect();
+	var sections = document.getElementsByTagName("section");
+
+	
+
 	function attachScroll() {
 		$(window).scroll(function(e) {
-			console.log("scroll");
 			scrollPage(e);
 		});
 	}
 
 	function detachScroll() {
-		$(window).unbind("scroll");
+		$(window).unbind("scroll");		
+		$(window).scrollable = false;
 	}
 
 	var lastScrollTop = $(window).scrollTop();
@@ -25,15 +30,24 @@ function Scroller() {
 
 		var $element;
 
-		console.log(lastScrollTop, $(this).scrollTop(), $(this).scrollTop() - lastScrollTop);
+		var windowHeight = window.innerHeight;
+		// console.log(sections[2].getBoundingClientRect().top);
+		// console.log(windowHeight * 0.8);
+		// console.log(windowHeight * 0.7);
 
-		if ($(this).scrollTop() < lastScrollTop) {
-			console.log("up");
-			$element = $("#lowcostholidays");
-		} else if ($(this).scrollTop() > lastScrollTop) {
-			console.log("down");
-			$element = $("#ruby");
-		}
+		for (var i = 0; i < sections.length; i++) {
+			if ((sections[i].getBoundingClientRect().top < (windowHeight * 0.8) && sections[i].getBoundingClientRect().top > (windowHeight * 0.6))) {
+				
+				console.log(sections[i], i, "down");
+				$element = $(sections[i]);
+			}
+			if ((sections[i].getBoundingClientRect().bottom < (windowHeight * 0.4) && sections[i].getBoundingClientRect().bottom > (windowHeight * 0.2))) {
+				
+				console.log(sections[i], i, "up");
+				$element = $(sections[i]);
+			}
+
+		};
 		
 		if ($element) {
 			$("html body").animate({
