7c11a9d12811ced899196c92cc4d3bf82d48ff45
Joseph Case <joseph@bluequest.co.uk>
2015-08-04 18:03:17 +0100

    end of day

--- js/Page.js
+++ js/Page.js
@@ -6,16 +6,52 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 function Page() {
 	$(".bubble").on( "click", function() {
-		if (!$(this).hasClass("active")) {
+		if ($("body").hasClass("initial")) {			
+			openHeader(this);
+		} else {
+			show(this);
+		}
+			 	
+	});
+
+	function openHeader(section) {
+		console.log("openHeader");
+		 $("body.initial").removeClass("initial");
+		 onTransitionEnd($("header")[0], function() {console.log("transition finished")}, section);
+	}
+
+	function show(section) {
+		console.log("show");
+		//need to make this more defensive
+		if (!$(section).hasClass("active")) {
 			$(".bubble.active").removeClass("active");
-		 	$(this).addClass("active");
-		 	$("body.initial").removeClass("initial");
+		 	$(section).addClass("active");
 			$('section.active').addClass("remove");
-			$('#' + this.getAttribute('data-section')).addClass("active");
+			$('#' + section.getAttribute('data-section')).addClass("active");
 			setTimeout(function() {
 				$('section.remove').removeClass("remove active");
 			}, 1200);
 		}
-	 	
-	});
+	}
+
+	function onTransitionEnd(element, func, aParams) {
+	    var transitionEnd;
+		// transition prefixes
+		var transitions = {
+	      'transition':'transitionend',
+	      'OTransition':'oTransitionEnd',
+	      'MozTransition':'transitionend',
+	      'WebkitTransition':'webkitTransitionEnd'
+	    }
+	    //loop through to check which is the right transition prefix (transition)
+	    for(t in transitions){
+	        if( element.style[t] !== undefined ){
+	            transitionEnd = transitions[t];
+	            break;
+	        }
+	    }
+	    $(element).on(transitionEnd, function() {
+	    	func(aParams);
+	    })
+	}
 }
