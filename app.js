14109f7e63ed77aad7061c40de20c41a821aa60e
Joseph Case <joseph.case@hotmail.co.uk>
2016-09-05 23:23:59 +0100

    added completed text and tweaked styling

--- app.js
+++ app.js
@@ -1,29 +1,55 @@
 document.addEventListener('DOMContentLoaded', DOMReady);
 
-var sections;
+var sections, videos;
+
+
+// add custom property of the HTML video object
+Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
+    get: function(){
+        return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
+    }
+})
 
 
 function DOMReady() {
+
 	sections = document.getElementsByTagName('section');
-	for (var i = sections.length - 1; i >= 0; i--) {
-		console.log(sections[i].offsetTop);
-		console.log(sections[i].offsetBottom);
-	}
+	videos = document.getElementsByTagName('video');
 
 	window.addEventListener('scroll', scrollHandler);
 
 }
 
-function scrollHandler(e) {
+function scrollHandler() {
+	sectionScroll();
+	videoScroll();
+}
 
+function sectionScroll() {
 	for (var i = sections.length - 1; i >= 0; i--) {
-		console.log(window.innerHeight)
 		if(window.scrollY < sections[i].offsetTop) {
 			sections[i].className = 'preStick';
 		}  else if (window.scrollY + window.innerHeight > sections[i].offsetTop + sections[i].offsetHeight)
 			sections[i].className = 'postStick';
 		else {
 			sections[i].className = 'stick';			
+		}
+	}
+}
+
+function videoScroll() {
+	for (var i = videos.length - 1; i >= 0; i--) {
+		var vidBounds = videos[i].getBoundingClientRect();
+		if(vidBounds.top + (0.5 * vidBounds.height) < window.innerHeight && vidBounds.bottom > 0) {
+			if(!videos[i].playing) {
+				console.log('play');
+				videos[i].play();
+			}
+		} else {
+			if(videos[i].playing) {
+				console.log('pause');
+				videos[i].pause();
+			}
 		}
 	}
 }
