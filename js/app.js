6edd3e846b9a7224ddfdcb8213f7f28d5a62a49b
Joseph Case <joseph.case@hotmail.co.uk>
2016-09-19 23:03:28 +0100

    finished for release
    - Fixed partical canvas
    - Crossed device fixes
    - Cross browser fixes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', DOMReady);
 
-var sections, videos, canvas = {};
+var sections, videos, canvas = {}, scrollTop, showHeader = true;
 
 
 // add custom property of the HTML video object
@@ -12,13 +12,18 @@ Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
 
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
+
+  	// hide the header
+  	document.getElementById('header').classList.add('hidden');
+
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
+      	showHeader = false;
         $('html, body').animate({
           scrollTop: target.offset().top
-        }, 500);
+        }, 500, function() {setTimeout(function() {showHeader = true}, 100)});
         return false;
       }
     }
@@ -27,41 +32,69 @@ $(function() {
 
 function DOMReady() {
 
+    var needles = new Needles(document.getElementById('particles'), '#383737', 100);
+
 	sections = document.getElementsByTagName('section');
 	videos = document.getElementsByTagName('video');
 	canvas.element = document.getElementById('particles');
 	canvas.ratio = canvas.element.width / canvas.element.height;
 
+	setSectionHeight();
+
 	window.addEventListener('scroll', scrollHandler);
 	window.addEventListener('resize', resizeHandler);
-	resizeHandler();
+
+	resizeHandler(); //handles the pixel canvas display and section heights
+
+
 
 }
 
-function scrollHandler() {
-	sectionScroll();
+function setSectionHeight() {	
+	sections[0].style.height = window.innerHeight + 'px'	;
+}
+
+function scrollHandler(e) {
+	headerScroll(e);
+	if(window.innerWidth > 900) {
+		sectionScroll();
+	}
 	videoScroll();
 }
 
 function resizeHandler() {
-	var scaleRatio;
-	if(window.innerWidth / window.innerHeight > canvas.ratio) {
-		scaleRatio = window.innerWidth / canvas.element.width;
-	} else {
-		scaleRatio = window.innerHeight / canvas.element.height;
+
+	// var scaleRatio;
+	// if(window.innerWidth / window.innerHeight > canvas.ratio) {
+	// 	scaleRatio = window.innerWidth / canvas.element.width;
+	// } else {
+	// 	scaleRatio = window.innerHeight / canvas.element.height;
+	// }
+	// console.log(scaleRatio);
+	// canvas.element.style.width = (canvas.element.width * scaleRatio) + "px";
+	// canvas.element.style.height = (canvas.element.height * scaleRatio) + "px";
+}
+
+function headerScroll(e) {
+
+	console.log($(window).scrollTop() == scrollTop);
+		
+	if($(window).scrollTop() > scrollTop || scrollTop < window.innerHeight || !showHeader) {
+		document.getElementById('header').classList.add('hidden');
+	} else if($(window).scrollTop() < scrollTop) {
+		document.getElementById('header').classList.remove('hidden');
 	}
-	console.log(scaleRatio);
-	canvas.element.style.width = (canvas.element.width * scaleRatio) + "px";
-	canvas.element.style.height = (canvas.element.height * scaleRatio) + "px";
+	scrollTop = $(window).scrollTop();
 }
 
 function sectionScroll() {
+	var scrollTop = $(window).scrollTop();
 	for (var i = sections.length - 1; i >= 0; i--) {
-		if(window.scrollY < sections[i].offsetTop) {
+		if(scrollTop < sections[i].offsetTop) {
 			sections[i].className = 'preStick';
-		}  else if (window.scrollY + window.innerHeight > sections[i].offsetTop + sections[i].offsetHeight)
+		} else if (scrollTop + window.innerHeight > sections[i].offsetTop + sections[i].offsetHeight) {
 			sections[i].className = 'postStick';
-		else {
+		} else {
 			sections[i].className = 'stick';			
 		}
 	}
@@ -70,7 +103,7 @@ function sectionScroll() {
 function videoScroll() {
 	for (var i = videos.length - 1; i >= 0; i--) {
 		var vidBounds = videos[i].getBoundingClientRect();
-		if(vidBounds.top + (0.5 * vidBounds.height) < window.innerHeight && vidBounds.bottom > 0) {
+		if(vidBounds.top + (0.5 * vidBounds.height) < window.innerHeight && vidBounds.bottom > 0 && videos[i].readyState === 4) {
 			if(!videos[i].playing) {
 				console.log('play');
 				videos[i].play();
--- js/particles.js
+++ js/particles.js
@@ -1,58 +1,79 @@
-var Needles = function(canvas, color, amount) {
+var Needles = function(canvas, color, density) {
 
-    var cw = canvas.width, ch = canvas.height, color = color,
+    canvas.width = canvas.offsetWidth, canvas.height = canvas.offsetHeight;
+
+    var cw = canvas.offsetWidth, ch = canvas.offsetHeight, color = color,
+
     mouseX = cw/2, mouseY = ch/2,
     ctx = canvas.getContext('2d');
-    ctx.lineWidth = 4,
+    // ctx.lineWidth = 1,
 	ctx.lineCap='round';	
     ctx.fillStyle = color;
+    
+    ctx.globalAlpha = 0.5;
     var stop = false;
 
     var pixels = [];
+    var introSection;
 
     var colors = ['#f3a0ae', '#2c5d8a', '#a22529'];
 
-    
+    console.log(density, cw, ch, 1e6)
+
+    var amount = density * cw * ch / 1e6;
 
     for (var i = 0; i < amount; i++) {
-    	pixels.push({x: Math.random() * cw, y: Math.random() * cw, color: colors[Math.floor(3*Math.random())]});
+    	pixels.push({x: Math.random() * cw, y: Math.random() * ch, color: colors[Math.floor(3*Math.random())]});
     }
 
-    draw();
+    // draw();
 
-    document.getElementsByTagName('section')[0].addEventListener('mousemove', function(e) {
+    introSection = document.getElementsByTagName('section')[0]
 
-        console.log(canvas.offsetWidth / canvas.width, canvas.offsetHeight / canvas.height);
+    introSection.addEventListener('mousemove', userActionHandler);
+    introSection.addEventListener('touchstart', userActionHandler);
+    introSection.addEventListener('touchmove', userActionHandler);
 
-        mouseX = e.pageX / (canvas.offsetWidth / canvas.width);
-        mouseY = e.pageY / (canvas.offsetHeight / canvas.height);
+    // window.addEventListener('resize', resizeHandler);
+
+    function resizeHandler() {
+        canvas.width = canvas.offsetWidth, canvas.height = canvas.offsetHeight;
+        ctx = canvas.getContext('2d');
+        // ctx.lineWidth = 1;
+        ctx.lineCap='round';
+    }
+
+    function userActionHandler(e) {
+        if(e && e.touches) {
+            mouseX = e.touches[0].pageX / (canvas.offsetWidth / canvas.width);
+            mouseY = e.touches[0].pageY / (canvas.offsetHeight / canvas.height);
+        } else {
+            mouseX = e.pageX / (canvas.offsetWidth / canvas.width);
+            mouseY = e.pageY / (canvas.offsetHeight / canvas.height);
+        }
         if(!stop) {
             window.requestAnimationFrame(draw);
         }
-
-    })
+    }
 
     function draw() {
 
         //clear the canvas
-        ctx.resetTransform();
         ctx.clearRect(0, 0, cw, ch);
 
         //Draw each particle
         for (var i = 0; i < pixels.length; i++) {
 
-        	var angle = Math.atan2(mouseY - pixels[i].y, mouseX - pixels[i].x);
-
-	        //reset position
-	        ctx.resetTransform();
+        	// var angle = Math.atan2(mouseY - pixels[i].y, mouseX - pixels[i].x);
 
 	        // get the new origin/angle
 	        ctx.translate(pixels[i].x, pixels[i].y);
-	        ctx.rotate(angle);
+
+            // console.log(pixels[i].x, pixels[i].y);
 
 	        //draw
-	        ctx.beginPath();
-            ctx.strokeStyle = pixels[i].color;
+	        // ctx.beginPath();
+            // ctx.strokeStyle = pixels[i].color;
 
 	        //LINE
             // ctx.moveTo(0 , 0);
@@ -62,19 +83,31 @@ var Needles = function(canvas, color, amount) {
             // ctx.lineTo((-(mouseY - pixels[i].y) / Math.sin(angle))/10 , 0);
 
             //BLOB
-            var lineWidth = 2000/((mouseY - pixels[i].y) / Math.sin(angle));
-            lineWidth = (lineWidth > 200) ? 200 : lineWidth;
-            ctx.lineWidth = lineWidth;
-	        ctx.moveTo(0 , 0);
-	        ctx.lineTo(0 , 0);
+            // var lineWidth = 1000/((mouseY - pixels[i].y) / Math.sin(angle));
+            // lineWidth = (lineWidth > 200) ? 200 : lineWidth;
+            // ctx.lineWidth = lineWidth;
+	        // ctx.lineTo(0 , 0);
 
 	        // TRIANGLE
-	        // ctx.moveTo(-3, -3);
-	        // ctx.lineTo(3 , 0);
-	        // ctx.lineTo(-3 , 3);
-	        // ctx.fill();
+            // ctx.lineTo(3 , 0);
+            // ctx.lineTo(-3 , 3);
+            // ctx.fill();
 
-	        ctx.stroke();
+            // ctx.stroke();
+
+
+            var size = 500/Math.sqrt(Math.pow(mouseX - pixels[i].x, 2) + Math.pow(mouseY - pixels[i].y, 2));
+            // size = (size > 500) ? 500 : size;
+
+	        ctx.moveTo(0, 0);
+            ctx.beginPath();
+            ctx.arc(0, 0, size, 0, 2*Math.PI, false);
+
+            // line color
+            ctx.fillStyle = pixels[i].color;
+            ctx.fill();
+
+            ctx.translate(-pixels[i].x, -pixels[i].y);
 
 
         } 
@@ -82,8 +115,7 @@ var Needles = function(canvas, color, amount) {
     }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    // var needles = new Needles(document.getElementById('particles'), '#d53300', 500);
-    var needles = new Needles(document.getElementById('particles'), '#383737', 200);
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//     // var needles = new Needles(document.getElementById('particles'), '#d53300', 500);
+// });
 
